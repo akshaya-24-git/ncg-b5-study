5642edeec9f16288f505658728136b438d0bc1cb
astraa-paul <p.samuel@saama.com>
2026-07-13 15:29:37 +0530

    add date-dim

--- dataform/definitions/param/source/date_ref.js
+++ dataform/definitions/param/source/date_ref.js
@@ -1,10 +1,26 @@
 const source = {
-    dk: ["'MTRXB5'", "calendar_date", "CURRENT_TIMESTAMP()"],
-    bk: ["'MTRXB5'", "calendar_date"],
-    source_table: "DATE_REF",
+    // Data Key: A composite primary key for the source record.
+    dk: [
+        "'MTRXB5'",
+        "full_date",
+        "fiscal_year"
+    ],
+    // Business Key:
+    bk: [
+        "full_date"
+    ],
+
+    // Incremental Load Column: 
+    //source_incr_load_column: "wt_timestamps",
+    // Full Read Flag:
+    source_full_read_flag: "Y",
+
+    // MODIFIED: Define source tables here
+    source_table_reference_date: "DATE_REF",
+
+    // Stage table and target table names.
+    stage_table: "step_tbl_process_date_dim",
     target_table: "date_dim"
 };
 
-module.exports = {
-    source
-};
+module.exports = { source };
--- dataform/definitions/param/target/date_dim.js
+++ dataform/definitions/param/target/date_dim.js
@@ -1,8 +1,18 @@
+// definitions/params/target_dim_date_params.js
+// This file defines the static metadata structure for the final dim_date target table.
+
 const target = {
+    // The name of the final table in your b5_HIS_t target data warehouse.
     target_table: "date_dim",
-    bk: ["date_dk"],
-    natural_key: "date_dk",
+
+    // THE unique dimension key to the final dimension table
     dk: ["date_dk"],
+
+    // The unique Business Key (Primary Key) for calendar integrity.
+    bk: ["full_date"],
+
+    // The complete list of all destination columns mapped out in your STTM.
+    // This array is used by your central functions to structure the final SELECT statement.
     columns: [
         "date_dk",
         "full_date",
@@ -18,6 +28,9 @@ const target = {
         "is_holiday",
         "is_festival"
     ],
+
+    // Since this is a static reference table, it performs an absolute load.
+    // We only expose a clean insert_list to populate all properties simultaneously.
     insert_list: [
         "date_dk",
         "full_date",
@@ -32,22 +45,8 @@ const target = {
         "is_weekend",
         "is_holiday",
         "is_festival"
-    ],
-    update_list: [
-        "full_date",
-        "day_of_week",
-        "day_of_week_num",
-        "week_of_year",
-        "month_num",
-        "month_name",
-        "quarter_num",
-        "year_num",
-        "fiscal_year",
-        "is_weekend",
-        "is_holiday",
-        "is_festival"
-    ],
-    target_dataset: "b5_HIS_t"
+    ]
 };
 
+// Export the target object so your dynamic write framework can read it
 module.exports = { target };
